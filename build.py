6d0c1305596f16213fd5d5a4a1ede1898899d133
William H. Grover <wgrover@gmail.com>
2024-03-16 15:48:59 -0700

    projects now in a simple textfile

--- build.py
+++ build.py
@@ -1,38 +1,21 @@
 
-projects = [
-    {
-        "name": "Pneumatic error detector",
-        "img": "error_detector.jpeg",
-        "links": [("arXiv", "https://arxiv.org/abs/2401.16500")]
-    },
-    {
-        "name": "Pressurama",
-        "img": "error_detector.jpeg",
-        "links": [("GitHub", "https://github.com/groverlab/pressurama")]
-    },
-    {
-        "name": "Vacuum-powered Intermittent Pneumatic Compression (IPC) device",
-        "img": "error_detector.jpeg",
-        "links": [("arXiv", "https://arxiv.org/abs/2401.16500")]
-    }
-]
-
-
+projectsfile = open("projects.txt", "r")
 infile1 = open("1.html", "r")
 infile2 = open("2.html", "r")
 outfile = open("index.html", "w")
 
 outfile.write(infile1.read())
-for p in projects:
+for project in projectsfile.read().split("\n\n"):
+    name, image, *links = project.split("\n")
     outfile.write('<div class="col"><div class="card shadow-sm">')
-    outfile.write(f'<img src="assets/{p["img"]}">')
+    outfile.write(f'<img src="assets/{image}">')
     outfile.write('<div class="card-body">')
-    outfile.write(f'<p class="card-text">{p["name"]}</p>')
+    outfile.write(f'<p class="card-text">{name}</p>')
     outfile.write('<div class="d-flex justify-content-between align-items-center"><div class="btn-group">')
-    for link in p["links"]:
-        outfile.write(f'<a href="{link[1]}">')
-        outfile.write(f'<button type="button" class="btn btn-sm btn-outline-secondary">{link[0]}</button>')
-        outfile.write('</a></div></div></div></div></div>')
+    for link in links:
+        outfile.write(f'<a href="{link.split()[1]}">')
+        outfile.write(f'<button type="button" class="btn btn-sm btn-outline-secondary">{link.split()[0]}</button>')
+    outfile.write('</a></div></div></div></div></div>')
 
 outfile.write(infile2.read())
 outfile.close()
