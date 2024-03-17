626d2b505862b437917b91d52efaa447a60ed772
William H. Grover <wgrover@gmail.com>
2024-03-16 18:43:41 -0700

    appearance fixes

--- build.py
+++ build.py
@@ -13,7 +13,6 @@ for project in projectsfile.read().split("\n\n"):
     outfile.write(f'<p class="card-text">{name}</p>')
     outfile.write('<div class="btn-group">')
     for link in links:
-        # outfile.write(f'<a href="{link.split()[1]}">')
         outfile.write(f'<a href="{link.split()[1]}" class="btn btn-outline-warning">{link.split()[0]}</a>')
     outfile.write('</a></div></div></div></div>')
 
