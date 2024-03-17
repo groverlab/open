314945139171ddfa06362eb60761c62457914f3f
William H. Grover <wgrover@gmail.com>
2024-03-16 17:13:57 -0700

    style edits

--- build.py
+++ build.py
@@ -11,11 +11,11 @@ for project in projectsfile.read().split("\n\n"):
     outfile.write(f'<img src="assets/{image}">')
     outfile.write('<div class="card-body">')
     outfile.write(f'<p class="card-text">{name}</p>')
-    outfile.write('<div class="d-flex justify-content-between align-items-center"><div class="btn-group">')
+    outfile.write('<div class="btn-group">')
     for link in links:
-        outfile.write(f'<a href="{link.split()[1]}">')
-        outfile.write(f'<button type="button" class="btn btn-sm btn-outline-secondary">{link.split()[0]}</button>')
-    outfile.write('</a></div></div></div></div></div>')
+        # outfile.write(f'<a href="{link.split()[1]}">')
+        outfile.write(f'<a href="{link.split()[1]}" class="btn btn-outline-warning">{link.split()[0]}</a>')
+    outfile.write('</a></div></div></div></div>')
 
 outfile.write(infile2.read())
 outfile.close()
